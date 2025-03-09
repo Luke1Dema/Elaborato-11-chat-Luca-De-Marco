02ba2922643a4acb7edc0e5492ea1e8161a9e202
lckde <luc.demarco1@edu.unifi.it>
2025-03-09 17:32:50 +0100

    Aggiunto controllo dei nomi utente per verificare che siano diversi prima di creare la chat

--- ChatRegistry.cpp
+++ ChatRegistry.cpp
@@ -8,21 +8,24 @@ void ChatRegistry::addChat(const Chat &chat) {
     std::string key = generateChatKey(chat.getUser1(), chat.getUser2());
     chats.insert(std::pair<std::string, Chat>(key, chat));
 }
-//sender e destinatatio diversi
+
 void ChatRegistry::addMessageToChat(const User &user1, const User &user2, const Message &message) {
-    std::string key = generateChatKey(user1.getUsername(), user2.getUsername());
+    if(user1.getUsername()!=user2.getUsername()){
+        std::string key = generateChatKey(user1.getUsername(), user2.getUsername());
 
-    // Si constrolla se la chat esiste già tramite find()
+        // Si constrolla se la chat esiste già tramite find()
 
-    auto it = chats.find(key);
-    if (it != chats.end()) {
-        it->second.addMessage(message); //si aggiunge il messaggio alla chat
-    }
-    else {
-        // Si crea una nuova chat se non esiste
-        Chat newChat(user1, user2);
-        newChat.addMessage(message);
-        chats.insert(std::pair<std::string, Chat>(key, newChat));
+        auto it = chats.find(key);
+
+        if (it != chats.end()) {
+            it->second.addMessage(message); //si aggiunge il messaggio alla chat
+        }
+        else {
+            // Si crea una nuova chat se non esiste
+            Chat newChat(user1, user2);
+            newChat.addMessage(message);
+            chats.insert(std::pair<std::string, Chat>(key, newChat));
+        }
     }
 }
 
